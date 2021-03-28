7e76ce66dab3e74e5e30df4e9eb9473374405bda
DaeyongK <>
2021-03-27 22:45:26 -0400

    Added initial html, created template, added index page

---
+++ src/style/master.js
@@ -0,0 +1,14 @@
+console.log("lll");
+$(document).ready(function (){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll > 10){
+            $("nav").removeClass("navTransparent");
+            $("nav").addClass("navColor");
+        }
+        else{
+            $("nav").removeClass("navColor");
+            $("nav").addClass("navTransparent");
+        }
+    })
+})
